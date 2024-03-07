ac2b1104c1d758fd52c8fbcbd33320b5c1c82c69
sarahhe05 <sarahhe725@gmail.com>
2024-03-07 13:35:50 -0800

    3/7/2024

--- extra.js
+++ extra.js
@@ -1,20 +1,10 @@
 function toggleGuardian() {
-    // get the clock
     var guardian = document.getElementById('the-guardian-section');
-
-    // get the current value of the clock's display property
     var displaySetting = guardian.style.display;
-
-    // also get the clock button, so we can change what it says
-    var guardianButton = document.getElementById('access-the-guardian-button');
-
-    // now toggle the clock and the button text, depending on current state
     if (displaySetting == 'block') {
-      // clock is visible. hide it
       guardian.style.display = 'none';
     }
     else {
-      // clock is hidden. show it
       guardian.style.display = 'block';
     }
   }
